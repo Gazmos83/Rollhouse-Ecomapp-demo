e051f1138391a2d2729b2d60af56dbd6f0d8ae28
gazmos83 <chimericanogmail.com>
2019-02-03 13:04:24 +0100

    reroute

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/index'
-  root 'simple_pages/index'
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
