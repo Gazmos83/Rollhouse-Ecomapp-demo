2d2a0be91846aff2eee6672e30778e795d3f0ea5
gazmos83 <chimericanogmail.com>
2019-02-03 13:26:30 +0100

    reroot to products homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/index'
-  root 'simple_pages#index'
+  get 'products/index'
+  get 'products/show'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
