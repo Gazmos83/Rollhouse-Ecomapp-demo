019fc2f71dddd3d54212225eefb0d8fa370bbfc1
gazmos83 <chimericanogmail.com>
2019-02-04 18:27:37 +0100

    4.5 carousel fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'products/index'
   get 'products/show'
-  root 'products#index'
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
