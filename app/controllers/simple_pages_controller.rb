70ed3164f1e935935099d4c5d9bcb3112232edd7
gazmos83 <chimericanogmail.com>
2019-02-05 19:34:41 +0100

    create carousel partial and simple_pages partial add on

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,8 +1,8 @@
 class SimplePagesController < ApplicationController
   def index
-    @products = Product.limit(3)
+    @products = Product.limit(6)
   end
   def landing_page
-    @products = Product.limit(3)
+    @products = Product.limit(6)
   end
 end
