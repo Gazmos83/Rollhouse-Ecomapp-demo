9a4e46f1d4a1453b7fa92e59ae8a6cd10c354825
gazmos83 <chimericanogmail.com>
2019-02-06 01:11:45 +0100

    colours column and contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index'
   get 'products/index'
   get 'products/show'
+  get 'simple_pages/contact'
   root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_01_130540) do
+ActiveRecord::Schema.define(version: 2019_02_05_234115) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2019_02_01_130540) do
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "color"
+    t.string "colour"
     t.string "price"
   end
 
