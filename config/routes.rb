7ba6f0762988e855bbfd28051715ddebef3d3dc9
gazmos83 <chimericano@gmail.com>
2019-02-08 00:39:09 +0100

    routes root page mod

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page'
 
 
-  root 'simple_pages#index'
+  root 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
