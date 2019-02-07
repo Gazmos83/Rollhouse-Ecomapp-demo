0a97fd982b59c54e220aad837d3ea0e226ebd07a
gazmos83 <chimericano@gmail.com>
2019-02-08 00:17:39 +0100

    5.1 reroute, new about, contact and faq pages & css clean-up

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   resources :users, except: [:index]
   get 'orders/index'
   get 'orders/show'
-  get 'simple_pages/index'
   get 'products/index'
   get 'products/show'
+  get 'simple_pages/about'
+  get 'simple_pages/faq'
+  get 'simple_pages/index'
   get 'simple_pages/contact'
   get 'simple_pages/landing_page'
-  root 'products#index'
+
+
+  root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
