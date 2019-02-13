8947b5b62d6f1967d484b7b702ce61e9cb4fa918
gazmos83 <chimericano@gmail.com>
2019-02-13 12:43:52 +0100

    general website clean-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
-  resources :users
+
   resources :products do
     resources :comments
   end
-
+  resources :users
 
 
   get 'simple_pages/products'
