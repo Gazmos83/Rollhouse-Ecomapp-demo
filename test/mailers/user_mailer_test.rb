0b488a31b2eaa7e4dc29c940405d08f48fce2006
gazmos83 <chimericano@gmail.com>
2019-02-15 18:27:30 +0100

    5.3 debug removed UserMailer files

--- test/mailers/user_mailer_test.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
-
-require 'test_helper'
-
-class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
