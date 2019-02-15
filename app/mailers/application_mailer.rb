05613d56dcef7fd3a292e3fb6637735fedb4ccb4
gazmos83 <chimericano@gmail.com>
2019-02-15 18:48:26 +0100

    user_mailer reinstall

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: 'chimericano@example.com'
+  layout 'mailer'
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
+  post 'simple_pages/thank_you'
 
   root 'simple_pages#landing_page'
 
