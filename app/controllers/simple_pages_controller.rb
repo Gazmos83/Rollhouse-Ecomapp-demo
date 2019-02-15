e24414b0595d0392e8bb179f36e8f232cbce4069
gazmos83 <chimericano@gmail.com>
2019-02-15 18:09:00 +0100

    5.3 remove mailer/heroku debug

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -9,15 +9,6 @@ class SimplePagesController < ApplicationController
     @featured_product = Product.first
   end
 
-  def thank_you
-   @name = params[:name]
-   @email = params[:email]
-   @message = params[:message]
-   UserMailer.contact_form(@email, @name, @message).deliver_now
-   ActionMailer::Base.mail(from: @email,
-     to: 'chimerican@gmail.com',
-     subject: "You're great #{@name}. Thanks for getting in touch",
-     body: @message).deliver_now
- end
+
 
 end
