49480569f56edca123677ca4e6a3578ac87e5cef
gazmos83 <chimericano@gmail.com>
2019-02-19 11:40:47 +0100

    product show page touch-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
 
+  # 5.8 nested resource
   resources :products do
     resources :comments
   end
