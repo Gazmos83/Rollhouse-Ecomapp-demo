41d56aa4cc8ba852ec019125e79357929c011212
gazmos83 <chimericano@gmail.com>
2019-02-19 22:15:47 +0100

    5.8 comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @product = @comment.product
+    @comment.destroy
+
+    respond_to do |format|
+      format.html { redirect_to @product, notice: 'Comment was successfully destroyed.' }
+      format.json { head :no_content }
   end
+end
 
   private
 	def comment_params
