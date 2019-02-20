1d65f627e292b42d9566a51376e6e3b977ec3de8
gazmos83 <chimericano@gmail.com>
2019-02-20 10:35:22 +0100

    5.10 Boolean flag

--- db/migrate/20190220092858_add_admin_flag_to_users.rb
+++ db/migrate/20190220092858_add_admin_flag_to_users.rb
@@ -1,6 +1,5 @@
 class AddAdminFlagToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :admin, :string
-    add_column :users, :, :boolean
+      add_column :users, :admin, :boolean, default: false, null: false
   end
 end
