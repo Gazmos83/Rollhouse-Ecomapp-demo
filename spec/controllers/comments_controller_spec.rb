205d21d8f4121f68db1cc26b0df92aa5a321a479
gazmos83 <chimericano@gmail.com>
2019-02-25 19:54:43 +0100

    6.3 Function tests

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+describe CommentsController, type: :controller do
+
+    before do
+        @user = FactoryBot.create(:user)
+        @comment = FactoryBot.create(:comment)
+        @product = FactoryBot.create(:product)
+    end
+
+    describe 'POST #create' do
+        context 'when a user is logged in' do
+            before do
+                sign_in @user
+            end
+
+            it "can create comments if user's account is confirmed" do
+                post :create, params: {comment: {title: @comment.title, body: @comment.body, rating: @comment.rating}, product_id: @product.id}
+                expect(response).to have_http_status(302)
+            end
+
+        end
+    end
+
+    describe 'DELETE #destroy' do
+        context 'when user is logged in' do
+            before do
+                sign_in @user
+            end
+
+            it "cannot delete comment if user is not an admin" do
+                delete :destroy, params: { product_id: @product.id, id: @comment.id }
+                expect(response).to have_http_status(302)
+            end
+
+            it "can delete comment if user is an admin" do
+                @user.admin = true
+                delete :destroy, params: { product_id: @product.id, id: @comment.id }
+                expect(response).to have_http_status(302)
+                expect(response).to redirect_to root_path
+            end
+        end
+    end
+
+end
---
+++ spec/controllers/orders_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe OrdersController, type: :controller do
+
+    before do
+        @user = FactoryBot.create(:user)
+        @user_second = FactoryBot.create(:user)
+        @order = FactoryBot.create(:order, user: @user)
+        @order_second = FactoryBot.create(:order, user: @user_second)
+    end
+
+    describe 'GET #show' do
+        context 'when a user is logged in' do
+            before do
+                sign_in @user
+            end
+
+            it "the user gets his orders" do
+                get :index
+                expect(response).to be_ok
+                expect(response).to render_template('index')
+            end
+
+        end
+
+        context 'when a user is not logged in' do
+            it "redirects to login" do
+                get :index
+                expect(response).to redirect_to new_user_session_path
+            end
+        end
+    end
+
+    describe 'DELETE #destroy' do
+        context 'when a user is logged in' do
+            before do
+                sign_in @user
+            end
+
+            it 'can delete own orders' do
+                delete :destroy, params: { id: @order.id }
+                expect(response).to have_http_status(302)
+            end
+
+            it 'cannot delete other users orders' do
+                delete :destroy, params: { id: @order_second.id }
+                expect(response).to have_http_status(302)
+                expect(response).to redirect_to root_path
+            end
+        end
+    end
+
+end
