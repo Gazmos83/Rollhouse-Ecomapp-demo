c9d124df06c366d0e72544473119d2b9cd5f0d25
gazmos83 <chimericano@gmail.com>
2019-02-27 22:04:26 +0100

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,13 +72,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for mailgun
   ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => "smtp.mailgun.org",
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
     :domain         => ENV['MAILGUN_SMTP_DOMAIN'],
     :user_name      => ENV['MAILGUN_SMTP_USERNAME'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :authentication => :plain,
   }
+  ActionMailer::Base.delivery_method = :smtp
 
   config.active_job.queue_adapter = :delayed_job
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
