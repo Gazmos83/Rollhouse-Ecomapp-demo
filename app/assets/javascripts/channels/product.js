8a34a3e7c758b8b15402930941e1f5de6c71fa11
gazmos83 <chimericano@gmail.com>
2019-03-07 16:20:10 +0100

    6.14 Active Job and Adv ActionCable

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -10,5 +10,17 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
   received: function() {
   // Called when there's incoming data on the websocket for this channel
   $(".alert.alert-info").show();
+  $('.product-reviews').prepend(data.comment);
+  $("#average-rating").attr('data-score', data.average_rating);
+  refreshRating();
+  },
+
+  // jQuery code selects element and then grabs the value of the data-product-id attribute
+  listen_to_comments: function() {
+  return this.perform('listen', {product_id: $("[data-product-id]").data("product-id")});
 }
+});
+  // Calls listen_to_comments when user opens page
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
