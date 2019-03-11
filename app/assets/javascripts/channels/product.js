48166e857603a1b4a1d70bc3f8753b00eb812b3b
gazmos83 <chimericano@gmail.com>
2019-03-11 15:01:12 +0100

    Uglifier syntax error fix

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -9,14 +9,7 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
 
   received: function() {
     // Called when there's incoming data on the websocket for this channel
-    $(".alert.alert-info").show();
-  },
-  listen_to_comments: function() {
 
-  }
-
-
-  // Called when there's incoming data on the websocket for this channel
   $(".alert.alert-info").show();
   $('.product-reviews').prepend(data.comment);
   $("#average-rating").attr('data-score', data.average_rating);
