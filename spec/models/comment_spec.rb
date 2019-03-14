feee201f965d8713dae9753153c92fa9928dfc59
gazmos83 <chimericano@gmail.com>
2019-03-14 17:52:30 +0100

    comment_spec with syntax error?

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,28 +1,32 @@
 require 'rails_helper'
 
     describe Comment do
+
       context "when the comment is being validated" do
-        it "comment is invalid without a body" do
+          it "comment is invalid without a body" do
           @comment = FactoryBot.build(:comment, body: nil)
           expect(@comment).not_to be_valid
         end
 
-        context "when the comment is being validated" do
+
+      context "when the comment is being validated" do
           it "comment is invalid without a user" do
-            @comment = FactoryBot.build(:comment, user: nil)
-            expect(@comment).not_to be_valid
+          @comment = FactoryBot.build(:comment, user: nil)
+          expect(@comment).not_to be_valid
         end
 
-        context "when the comment is being validated" do
+
+      context "when the comment is being validated" do
           it "comment is invalid without a product" do
-            @comment = FactoryBot.build(:comment, product: nil)
-            expect(@comment).not_to be_valid
+          @comment = FactoryBot.build(:comment, product: nil)
+          expect(@comment).not_to be_valid
         end
 
-        context "when the comment is being validated" do
-          it "comment is invalid without a body" do
-            @comment = FactoryBot.build(:comment, rating: nil)
-            expect(@comment).not_to be_valid
-        end
-      end
+
+      context "when the comment is being validated" do
+          it "comment is invalid without a rating" do
+          @comment = FactoryBot.build(:comment, rating: nil)
+          expect(@comment).not_to be_valid
+       end
     end
+end
