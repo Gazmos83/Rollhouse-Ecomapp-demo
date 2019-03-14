03a417a72cbce297522c764517284243c092c1ab
gazmos83 <chimericano@gmail.com>
2019-03-14 17:53:37 +0100

    product_spec with search bar test update

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
       describe Product do
 
         context "when the product has comments" do
-          let(:product) { Product.create!(name: "race bike")}
-          let(:user) { User.create!(email: "kevin.louis.dath@gmail.com", password: "123456")}
+          let(:product) { Product.create!(name: "Malapascua")}
+          let(:user) { User.create!(email: "monkey@gmail.com", password: "123456")}
           before do
 
             product.comments.create!(rating: 1, user: user, body: "I hate this place")
@@ -12,13 +12,18 @@ require 'rails_helper'
             product.comments.create!(rating: 5, user: user, body: "I had such a good time!")
           end
 
-          it "returns the average rating of all comments" do
-
-             expect(product.average_rating).to eq 3
+            it "returns the average rating of all comments" do
+            expect(product.average_rating).to eq 3
           end
 
-          it "is not valid without a comment" do
-            expect(Product.new(description: "this place is beautiful")).not_to be_valid
+           it "is not valid without a comment" do
+           expect(Product.new(description: "this place is beautiful")).not_to be_valid
+          end
+
+      describe '#search' do
+           it "returns the product that matches the search term" do
+           expect(Product.search('Mala').first).to eq(product)
           end
       end
+    end
 end
