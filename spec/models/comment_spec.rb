39f3196363f2d9e9e20be2a70c787173f7bfeca5
gazmos83 <chimericano@gmail.com>
2019-03-14 17:12:11 +0100

    comment_spec test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,1 +1,28 @@
 require 'rails_helper'
+
+    describe Comment do
+      context "when the comment is being validated" do
+        it "comment is invalid without a body" do
+          @comment = FactoryBot.build(:comment, body: nil)
+          expect(@comment).not_to be_valid
+        end
+
+        context "when the comment is being validated" do
+          it "comment is invalid without a user" do
+            @comment = FactoryBot.build(:comment, user: nil)
+            expect(@comment).not_to be_valid
+        end
+
+        context "when the comment is being validated" do
+          it "comment is invalid without a product" do
+            @comment = FactoryBot.build(:comment, product: nil)
+            expect(@comment).not_to be_valid
+        end
+
+        context "when the comment is being validated" do
+          it "comment is invalid without a body" do
+            @comment = FactoryBot.build(:comment, rating: nil)
+            expect(@comment).not_to be_valid
+        end
+      end
+    end
