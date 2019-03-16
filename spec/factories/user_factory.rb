d98fce49bc01a639253755e5a52017924c9cb6c9
gazmos83 <chimericano@gmail.com>
2019-03-16 15:37:59 +0100

    new admin user

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -7,4 +7,12 @@ FactoryBot.define do
     admin false
   end
 
+  factory :admin, class: User do
+    email {"peter@example.com"}
+    password {"1234567890"}
+    first_name {"Peter"}
+    last_name {"Fox"}
+    admin true
+  end
+
 end
